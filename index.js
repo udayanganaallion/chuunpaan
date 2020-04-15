557a95f97806526c59a86fcf372f099463b3d8b2
Gihan Wijerathna <gihan@alliontechnologies.com>
2020-04-15 17:30:43 +0530

    Index.js modified.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
-import "./style.css";
+import "./src/css/style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
 
