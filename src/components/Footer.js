774a06c428e40bb6a2c9ca151b6b4074dab5c283
Udayangana Bandara Gajanayaka <udayangana@alliontechnologies.local>
2020-04-17 22:19:20 +0530

    nav

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,5 +2,12 @@ import React, { Component } from "react";
 import { render } from "react-dom";
 
 export default class Footer extends Component {
-  
+    render() {
+          return (
+           
+            <div className="container text-center">
+              Copyrights Allion Technologies (2020 React Hackothon)
+            </div>
+          )
+        }
 }
--- src/index.js
+++ src/index.js
@@ -43,7 +43,7 @@ class App extends Component {
             </Route>
           </Switch>
 		    </div>
-        {/* <Footer /> */}
+        <Footer />
       </div>
     );
   }
