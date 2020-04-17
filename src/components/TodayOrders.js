b6be935c04e03327342ac80e041328a98dcb757e
wenura <wenura@alliontechnologies.com>
2020-04-17 21:25:36 +0530

    Total Orders

--- src/components/TodayOrders.js
+++ src/components/TodayOrders.js
@@ -6,6 +6,52 @@ import { Button, Modal, Table } from "react-bootstrap";
 
 export default class TodayOrders extends Component {
 
-  
+  constructor(props) {
+    super(props);
+    this.state = this.initialState;
+  }
 
+  get initialState() {
+    let myOrders = localStorage.getItem("myorders");
+    let items = [];
+    if (myOrders) {
+      items = JSON.parse(myOrders);
+    }
+    return {
+      items: items
+    };
+  }
+
+  itemsList() {
+    return this.state.items.map((item) => {
+      return (
+        <tr>
+              <td>{user.name}</td>
+              <td>{item.name}</td>
+              <td>{item.qty}</td>
+              <td>{item.price}</td>
+        </tr>
+      );
+    });
+  }
+  
+  render() {
+    return (
+      <div className="row">
+        <div className="col-md-12">
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Person Name</th>
+                <th>Item Name</th>
+                <th>Qty</th>
+                <th>Price</th>
+              </tr>
+            </thead>
+            {this.itemsList()}
+          </Table>
+        </div>
+      </div>
+    );
+  }
 }
