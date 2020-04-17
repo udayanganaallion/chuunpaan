81cc04eee238b9de5225e8f300d7af67847f9496
sanjeewasap <sanjeewa.sap@gmail.com>
2020-04-17 20:42:37 +0530

    Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,9 +9,20 @@ export default class Login extends Component {
     super(props)
   
     this.state = {
-      username:'',
-      password:''
+      username:''
     }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {    
+    this.setState({username: event.target.value});  
+  }
+
+  handleSubmit(event) {
+    if (this.state.username !== '') {
+      this.props.userLogin(this.state.username);
+    }    
   }
 
   render() {
@@ -19,7 +30,20 @@ export default class Login extends Component {
       <div className="container">
         <div className="row">
           <div className="col-md-12">
-            <h1>Login</h1>
+            <form onSubmit={event => {
+              event.preventDefault()
+              this.handleSubmit(event)
+            }} className="form-signin">
+                <h1 class="h3 mb-3 font-weight-normal text-center">Guest Login</h1>
+              <div className="form-group">
+                <label for="name" class="sr-only">User name</label>
+                <input type="text" value={this.state.username} onChange={this.handleChange} className="form-control" placeholder="Type user name" required></input>
+                <div class="invalid-feedback">Please enter your name</div>
+              </div>
+              <div className="form-group">
+                <button className="btn btn-lg btn-primary btn-block mt-3" type="submit">Login</button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
